4169f1517780f60bb99b5aba7f5842633437acad
Grayson Cunningham <gecun289@gmail.com>
2020-10-20 09:16:11 -0600

    Create apartment class with initialize and add_renter method

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -4,11 +4,11 @@ require './lib/renter'
 
 class RenterTest < Minitest::Test
   def setup
-    @renter_1 = Renter.new("Jessie")
+    @renter1 = Renter.new("Jessie")
   end
 
   def test_it_exists
-    assert_instance_of Renter, @renter_1
-    assert_equal "Jessie", @renter.name
+    assert_instance_of Renter, @renter1
+    assert_equal "Jessie", @renter1.name
   end
 end
